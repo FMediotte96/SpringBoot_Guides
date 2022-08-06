e7fd8134fabe87b7b9a6e281710309b13807ab86
Facu Mediotte <facumediotte@gmail.com>
2022-08-05 23:20:20 -0300

    Securing-web

---
+++ securing-web/securing-web/src/test/java/api/security/app/SecuringWebApplicationTests.java
@@ -0,0 +1,15 @@
+package api.security.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
