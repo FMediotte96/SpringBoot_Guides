a79edb739e3ee542595bdb2218eda0fb155a4009
Facu Mediotte <facumediotte@gmail.com>
2022-08-06 00:15:40 -0300

    Async-method

---
+++ async-method/async-method/src/test/java/api/async/app/AsyncMethodApplicationTests.java
@@ -0,0 +1,15 @@
+package api.async.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
