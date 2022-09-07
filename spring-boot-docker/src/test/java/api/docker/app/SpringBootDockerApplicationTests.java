bc42468deda56a00ddb2fe0499aedbb7331e620a
Facu Mediotte <facumediotte@gmail.com>
2022-09-07 20:48:26 -0300

    SpringBoot + Docker

---
+++ spring-boot-docker/src/test/java/api/docker/app/SpringBootDockerApplicationTests.java
@@ -0,0 +1,15 @@
+package api.docker.app;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
